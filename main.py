46ba06f55042f1fe633f3e06893dde181885585b
xxanqw <potik217@gmail.com>
2024-06-28 17:04:35 +0300

    Fixed icons folder

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from windows.mainwindow import MainWindow
 from os import path as p, mkdir
 if not p.exists(p.dirname(p.abspath(__file__)) + "/img/icnoutput"):
     mkdir(p.dirname(p.abspath(__file__)) + "/img/icnoutput")
+if not p.exists(p.dirname(p.abspath(__file__)) + "/img/icnoutput/icons"):
+    mkdir(p.dirname(p.abspath(__file__)) + "/img/icnoutput/icons")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
